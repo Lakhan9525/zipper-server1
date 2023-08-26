56ac5920a72f30c6b61b7b70ba9899f6bf7c9712
Lakhan9525 <lakhanraj987@gmail.com>
2023-08-26 11:34:16 +0530

    adedeon

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://c1067-zss.vercel.app/",
   })
 );
 app.use(bodyParser.json());
@@ -523,7 +523,7 @@ app.get("/api/profile", (req, res) => {
     });
   } else {
     res.json(null);
-    
+
   }
 });
 
